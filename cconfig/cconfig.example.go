3b804f7079825fdb41816678caf9888cb3d74957
nikolasmelui <slak@samaradom.ru>
2020-06-04 02:35:04 +0400

    Create customconfig module with example file

---
+++ cconfig/cconfig.example.go
@@ -0,0 +1,14 @@
+package cconfig
+
+// type customConfig struct {
+// 	BaseURL           string
+// 	BasicAuthLogin    string
+// 	BasicAuthPassword string
+// }
+
+// // Config ...
+// var Config = &customConfig{
+// 	BaseURL:           "https://your.xml.server.go",
+// 	BasicAuthLogin:    "login",
+// 	BasicAuthPassword: "password",
+// }

bc5094da949144905661a50f7de5d3d608ffa187
nikolasmelui <slak@samaradom.ru>
2020-06-08 17:41:09 +0400

    Add the Set method to the redis-cache.go file

--- cache/redis-cache.go
+++ cache/redis-cache.go
@@ -1,22 +1,23 @@
 package cache
 
 import (
+	"encoding/json"
 	"time"
 
 	"github.com/go-redis/redis/v8"
+	"github.com/nikolasmelui/go-xml2json-mapper/entity"
+	"github.com/nikolasmelui/go-xml2json-mapper/helper"
 )
 
-//RedisCache ...
-type RedisCache struct {
+type redisCache struct {
 	host     string
 	password string
 	db       int
 	expires  time.Duration
 }
 
-// NewRedisCache ...
-func NewRedisCache(host string, password string, db int, expires time.Duration) *RedisCache {
-	return &RedisCache{
+func newRedisCache(host string, password string, db int, expires time.Duration) *redisCache {
+	return &redisCache{
 		host:     host,
 		password: password,
 		db:       db,
@@ -24,11 +25,24 @@ func NewRedisCache(host string, password string, db int, expires time.Duration) 
 	}
 }
 
-// GetClient ...
-func (cache *RedisCache) GetClient() *redis.Client {
+func (cache *redisCache) getClient() *redis.Client {
 	return redis.NewClient(&redis.Options{
 		Addr:     cache.host,
 		Password: cache.password,
 		DB:       cache.db,
 	})
+}
+
+func (cache *redisCache) Set(key string, value *entity.Product) {
+	client := cache.getClient()
+
+	json, err := json.Marshal(&ProductCache{
+		Data: *value,
+		Hash: helper.InstanceHash(value),
+	})
+	if err != nil {
+		panic(err)
+	}
+	client.Set(nil, key, json, cache.expires*time.Second)
+
 }

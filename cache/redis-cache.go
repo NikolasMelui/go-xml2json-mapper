480225e81cabe24be1ce8ea10f658413f277fa83
nikolasmelui <slak@samaradom.ru>
2020-06-08 17:09:55 +0400

    Add realization of the GetClient method

--- cache/redis-cache.go
+++ cache/redis-cache.go
@@ -2,24 +2,33 @@ package cache
 
 import (
 	"time"
+
+	"github.com/go-redis/redis/v8"
 )
 
 //RedisCache ...
 type RedisCache struct {
 	host     string
-	port     int
-	db       string
 	password string
+	db       int
 	expires  time.Duration
 }
 
 // NewRedisCache ...
-func NewRedisCache(host string, port int, db string, password string, expires time.Duration) *RedisCache {
+func NewRedisCache(host string, password string, db int, expires time.Duration) *RedisCache {
 	return &RedisCache{
 		host:     host,
-		port:     port,
-		db:       db,
 		password: password,
+		db:       db,
 		expires:  expires,
 	}
+}
+
+// GetClient ...
+func (cache *RedisCache) GetClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     cache.host,
+		Password: cache.password,
+		DB:       cache.db,
+	})
 }
